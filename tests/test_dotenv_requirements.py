66f989e6e785446c77b515063641b7f79affd0a5
KanG98 <yankang198@gmail.com>
2022-08-15 20:41:38 -0400

    Add dotenv testing

--- tests/test_dotenv_requirements.py
+++ tests/test_dotenv_requirements.py
@@ -1,5 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 def test_dotenv():
-  print(os.getenv("EMAIL_PASSWORD"))
-  pass
+  load_dotenv()
+  all_envs_keys = os.environ.keys()
+  all_dotenv_env = []
+  with open("DOTENV", "r") as dotenv:
+    for env in dotenv:
+      all_dotenv_env.append(env)
+
+  assert all(env in all_envs_keys for env in all_dotenv_env)
