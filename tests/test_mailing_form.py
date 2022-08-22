bb2615bbf3ec0fd03bee032cf69d9fa410e27712
KanG98 <yankang198@gmail.com>
2022-08-22 00:02:23 -0400

    Test Firefox

--- tests/test_mailing_form.py
+++ tests/test_mailing_form.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,10 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def test_mailing_form():
   
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+  # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+  driver = webdriver.Firefox()
   driver.get("file:///"+os.getcwd()+"/client/index.html")
-  elem = driver.find_element(By.ID, "q")
-  elem.send_keys("pycon")
+  elem = driver.find_element(By.ID, "email")
+  elem.send_keys("hi")
   elem.send_keys(Keys.RETURN)
   assert "No results found." not in driver.page_source
   driver.close()
