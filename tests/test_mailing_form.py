f443aa9b3718f8c76e4e60aaf295c79698818446
KanG98 <yankang198@gmail.com>
2022-08-22 17:05:43 -0400

    Fix jenkins exception

--- tests/test_mailing_form.py
+++ tests/test_mailing_form.py
@@ -9,8 +9,8 @@ def test_mailing_form():
 
   service = Service(executable_path=ChromeDriverManager().install())
   opts = Options()
-  opts.add_argument('--no-sandbox')
   opts.add_argument('--headless')
+  opts.add_argument('--no-sandbox')
   opts.add_argument('--disable-dev-shm-usage')
   browser = webdriver.Chrome(service=service, options=opts)
   browser.get('http://google.com/')
